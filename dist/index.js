d75de98de6cf597e722a1c44d28ef6bb8a92b90b
zys8119 <770959294@qq.com>
2023-08-17 16:56:41 +0800

    [add] useram

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+var install = function (app) {
+    console.log(app, 4454554545);
+};
+var index = {
+    install: install
+};
+
+export { index as default };
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {App} from "vue"
+const install = (app:App)=>{
+    console.log(app, 4454554545)
+}
+export default {
+    install
+}
